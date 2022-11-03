d55b04d4f52c3df340ddb0de7ecefe3ab41c7bd9
Jorge Munera <gerjo9211@hotmail.com>
2022-11-03 12:08:30 -0500

    commit hasta mitad del curso

---
+++ Java/Main.java
@@ -0,0 +1,7 @@
+package Java;
+
+class Main{//en el caso de java necesitamos que el nombre de la clase tenga el mismo nombe del archivo
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
